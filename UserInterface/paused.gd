2d35718aa3667a36a6c6bd92d8839b18e39777bf
simigdalius <aggelikhsimigdali@gmail.com>
2026-03-29 16:16:35 -0400

    gamw

--- UserInterface/paused.gd
+++ UserInterface/paused.gd
@@ -1,23 +1,43 @@
 extends Control
-@onready var music = $music
-@onready var click =$swstoclick
+
+var cursor_scene = preload("res://UserInterface/cursor.tscn")
+var current_cursor = null
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	hide()
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+
+func toggle_pause():
+	get_tree().paused = !get_tree().paused
+	
+	if get_tree().paused:
+		current_cursor = cursor_scene.instantiate()
+		add_child(current_cursor) 
+		
+		show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		unpause()
+
 func unpause():
-	$".".hide()
+	# 2. Πλήρης διαγραφή του ποντικιού
+	if current_cursor:
+		current_cursor.queue_free()
+		current_cursor = null
+	
+	hide()
 	get_tree().paused = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	music.play()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_2_pressed() -> void:
+func _on_button_2_pressed():
 	unpause()
 
-
-func _on_button_3_pressed() -> void:
-	click.play()
+func _on_button_3_pressed():
+	if current_cursor:
+		current_cursor.queue_free()
+	get_tree().paused = false
 	Scenetransition.change_scene("res://UserInterface/mainMenu.tscn")
